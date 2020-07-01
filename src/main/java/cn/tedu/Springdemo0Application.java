40e4920d769677c341d195b8995e18f49a791870
Administrator <2840721384@qq.com>
2020-07-01 21:07:26 +0800

    idea第一次提交

---
+++ src/main/java/cn/tedu/Springdemo0Application.java
@@ -0,0 +1,13 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdemo0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springdemo0Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tedu/Springdemo0ApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.tedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdemo0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello spring!");
+    }
+
+}
